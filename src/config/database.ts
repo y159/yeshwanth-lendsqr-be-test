a4aca758aae98adc2d018869aa64332fd4b48894
y159 <yellanki.yeshwanth@gmail.com>
2026-05-26 16:18:40 -0500

    fix: move knexfile to src/config and fix build output structure

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,8 +1,7 @@
 import knex from "knex";
-import config from "../../knexfile";
+import config from "./knexfile";
 
-const environment =
-  process.env.NODE_ENV === "test" ? "development" : process.env.NODE_ENV || "development";
+const environment = process.env.NODE_ENV === "test" ? "development" : process.env.NODE_ENV || "development";
 
 const database = knex(config[environment]);
 
