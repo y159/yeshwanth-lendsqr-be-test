b82dceb31f23d6836d64077b50b221de8f4d673a
y159 <yellanki.yeshwanth@gmail.com>
2026-05-26 16:34:54 -0500

    Fix knex ESM import issue

--- src/config/knexfile.ts
+++ src/config/knexfile.ts
@@ -1,9 +1,8 @@
-import type { Knex } from "knex";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const config: { [key: string]: Knex.Config } = {
+const config = {
   development: {
     client: "mysql2",
     connection: {
