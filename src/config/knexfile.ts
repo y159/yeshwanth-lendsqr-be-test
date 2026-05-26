7b1dd62a19a77458069346cb76b35f4cc3d77785
y159 <yellanki.yeshwanth@gmail.com>
2026-05-26 16:49:05 -0500

    fix: update knexfile with production config and correct migrations path

--- src/config/knexfile.ts
+++ src/config/knexfile.ts
@@ -1,5 +1,4 @@
 import dotenv from "dotenv";
-
 dotenv.config();
 
 const config = {
@@ -13,8 +12,20 @@ const config = {
       database: process.env.DB_NAME,
     },
     migrations: {
-      directory: "./src/database/migrations",
-      extension: "ts",
+      directory: __dirname + "/../database/migrations",
+    },
+  },
+  production: {
+    client: "mysql2",
+    connection: {
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    },
+    migrations: {
+      directory: __dirname + "/../database/migrations",
     },
   },
 };
